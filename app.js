e24130a29da7a7d96a51a437485bf591bf4090a8
TedNistor <ted_nistor@yahoo.com>
2022-11-30 12:18:21 +0200

    Create initial layout

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const tailwindConfig = require("./tailwind.config");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
+app.use(express.static("public"));
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
