cb034bc8f8064e8e7ca2a8f45e5ce2cbc7bc1bcd
TedNistor <ted_nistor@yahoo.com>
2022-12-04 12:34:32 +0200

    add index.html to public dir

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
+app.get("/", (req, res) => res.sendFile(__dirname + "/public/index.html"));
 
 app.use(express.static("public"));
 
